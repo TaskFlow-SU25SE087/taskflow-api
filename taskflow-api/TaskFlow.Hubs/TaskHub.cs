1d86d3f48e10bb467be827e17adff7cf8f33f286
damduy <80811184+damduy@users.noreply.github.com>
2025-07-22 10:48:33 +0700

    Remove unused function in taskhub

--- taskflow-api/TaskFlow.Hubs/TaskHub.cs
+++ taskflow-api/TaskFlow.Hubs/TaskHub.cs
@@ -5,15 +5,6 @@ namespace taskflow_api.TaskFlow.API.Hubs
 {
     public class TaskHub : Hub
     {
-        // Optional: group users by project
-        public async Task JoinProjectGroup(string projectId)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, projectId);
-        }
-
-        public async Task LeaveProjectGroup(string projectId)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, projectId);
-        }
+        
     }
 }
